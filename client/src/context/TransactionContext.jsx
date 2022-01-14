6e0d483ad3e6941acd7af4566f6bc0725f80703a
Developer Junaid <qjunaid623@gmail.com>
2022-01-14 15:09:43 +0500

    Update TransactionContext.jsx

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -39,7 +39,7 @@ export const TransactionsProvider = ({ children }) => {
           timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
           message: transaction.message,
           keyword: transaction.keyword,
-          amount: parseInt(transaction.amount._hex) / 10 ** 18
+          amount: parseInt(transaction.amount._hex) / (10 ** 18)
         }));
 
         console.log(structuredTransactions);

accdb05907e83fb82965d280d573807027af79a8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-03 16:48:42 +0800

    Delete client directory

--- client/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-import abi from "./Transactions.json";
-
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
-export const contractABI = abi.abi;
--- client/src/utils/shortenAddress.js
+++
@@ -1,1 +0,0 @@
-export const shortenAddress = (address) => `${address.slice(0, 5)}...${address.slice(address.length - 4)}`;

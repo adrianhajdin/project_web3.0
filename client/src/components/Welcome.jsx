2f7605c8e961ef1e03d5f69e91395fb5ad1025db
Crazygit <lianglin999@gmail.com>
2022-02-22 11:45:30 +0800

    fix typos

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -75,7 +75,7 @@ const Welcome = () => {
         </div>
 
         <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">

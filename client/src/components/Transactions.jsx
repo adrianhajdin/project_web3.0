8f5a72f782bd693210e1870c21d7bd42f3ab0baa
Muhammad Umar Farooq <92017762+UmarFarooq361@users.noreply.github.com>
2023-05-06 14:09:02 +0500

    update recommended testnet to sepolia testnet

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -6,11 +6,20 @@ import useFetch from "../hooks/useFetch";
 import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
 
-const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+const TransactionsCard = ({
+  addressTo,
+  addressFrom,
+  timestamp,
+  message,
+  keyword,
+  amount,
+  url,
+}) => {
   const gifUrl = useFetch({ keyword });
 
   return (
-    <div className="bg-[#181918] m-4 flex flex-1
+    <div
+      className="bg-[#181918] m-4 flex flex-1
       2xl:min-w-[450px]
       2xl:max-w-[500px]
       sm:min-w-[270px]
@@ -20,11 +29,23 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
     >
       <div className="flex flex-col items-center w-full mt-3">
         <div className="display-flex justify-start w-full mb-6 p-2">
-          <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
-            <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
+          <a
+            href={`https://sepolia.etherscan.io/address/${addressFrom}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="text-white text-base">
+              From: {shortenAddress(addressFrom)}
+            </p>
           </a>
-          <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
-            <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
+          <a
+            href={`https://sepolia.etherscan.io/address/${addressTo}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="text-white text-base">
+              To: {shortenAddress(addressTo)}
+            </p>
           </a>
           <p className="text-white text-base">Amount: {amount} ETH</p>
           {message && (

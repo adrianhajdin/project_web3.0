01ff2916dba31a57911bf7b59e495628c5d04b7f
Prathmesh Aghao <89900988+prathmeshaghao@users.noreply.github.com>
2023-08-27 16:08:05 +0530

    Update hardhat.config.js
    changed importing module and test network

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,9 +1,9 @@
-require('@nomiclabs/hardhat-waffle');
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
+    goerli: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
       accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
     },
